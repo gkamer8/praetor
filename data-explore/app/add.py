4d46990e61afa56351cabafd5f27e333476884bc
Gordon Kamer <gkamer@college.harvard.edu>
2023-03-21 13:09:18 -0400

    before js

--- data-explore/app/add.py
+++ data-explore/app/add.py
@@ -5,7 +5,8 @@ from flask import (
     redirect
 )
 from app.db import get_db
-from app.db_wrappers import add_prompt
+from app.db_wrappers import add_prompt, add_bulk
+import json
 
 bp = Blueprint('add', __name__)
 
@@ -13,12 +14,36 @@ bp = Blueprint('add', __name__)
 def add():
     
     if request.method == "POST":
-        prompt = request.form.get("prompt")
-        style = request.form.get("style")
-        tags = request.form.get("tags")
-        db = get_db()
-        id = add_prompt(db, {'prompt': prompt, 'style': style, 'tags': tags})
-        return redirect(f"/view?prompt_id={id}")
+
+        # Was it a bulk upload?
+        file = request.files['file']
+        if file:
+            file_contents = file.read()
+            # Read as json
+            # btw this is probably a security vulnerability
+            # Probably should check for filesize as well
+            data = json.loads(file_contents)
+
+            # Optional
+            key_tags = request.form.get("key_tags")
+            if not key_tags:
+                key_tags = None
+            options = {
+                'key_completion': request.form.get("key_completion"),
+                'key_prompt': request.form.get("key_prompt"),
+                'key_tags': request.form.get("key_tags"),
+                'tags': request.form.get("tags"),
+                'style': request.form.get("style"),
+            }
+            add_bulk(get_db(), data, options)
+            return redirect(f"/manifest")
+        else:
+            prompt = request.form.get("prompt")
+            style = request.form.get("style")
+            tags = request.form.get("tags")
+            db = get_db()
+            id = add_prompt(db, {'prompt': prompt, 'style': style, 'tags': tags})
+            return redirect(f"/view?prompt_id={id}")
 
 
     return render_template('add.html')
--- data-explore/app/db_wrappers.py
+++ data-explore/app/db_wrappers.py
@@ -12,7 +12,7 @@ def add_example(db, input_dict):
     c = db.cursor()
     c.execute("INSERT INTO examples (completion, tags, prompt_id) VALUES (?, ?, ?)", (txt, tags, prompt_id))
     item_id = c.lastrowid
-    c.commit()
+    db.commit()
     return item_id
 
 # Adds prompt to database and returns that prompt's id
@@ -25,3 +25,39 @@ def add_prompt(db, input_dict):
     item_id = c.lastrowid
     db.commit()
     return item_id
+
+# Takes data and inserts prompts and examples
+# Data is a list of dictionaries 
+"""
+options takes:
+- (optional) tags (additional tags to append to each prompt and example)
+- (required) style (for the prompts)
+- (required) key_continuation (what key to use for the coninuation)
+- (required) key_prompt (what key to use for the prompt)
+- (optional) key_tags (what key to use for both prompt and example tags)
+"""
+def add_bulk(db, data, options):
+    key_completion = options['key_completion']
+    key_prompt = options['key_prompt']
+    key_tags = None if 'key_tags' not in options else options['key_tags']
+    style = options['style']
+    c = db.cursor()
+    for item in data:
+        prompt = item[key_prompt]
+        existing_prompt = c.execute(
+            'SELECT * FROM prompts WHERE prompt LIKE ?', (prompt,)
+        ).fetchone()
+        
+        if existing_prompt:
+            row_id = c.lastrowid
+        else:
+            tags = options['tags'] + (("," + item[key_tags]) if key_tags else "")
+            c.execute("INSERT INTO prompts (prompt, tags, style) VALUES (?, ?, ?)", (prompt, tags, style))
+            row_id = c.lastrowid
+            db.commit()
+        
+        # Now add example
+        txt = item[key_completion]
+        tags = options['tags'] + (("," + item[key_tags]) if key_tags else "")
+        c.execute("INSERT INTO examples (completion, tags, prompt_id) VALUES (?, ?, ?)", (txt, tags, row_id))
+        db.commit()
